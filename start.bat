286b8e8929aeeae35de55f9343308c994a709305
Lin, Ming Chih <mingchih.lin8@gmail.com>
2025-07-06 06:28:30 +0800

    Add start.bat for launching web-service

---
+++ start.bat
@@ -0,0 +1,21 @@
+@echo off
+
+if not exist "venv\Scripts\activate.bat" (
+    echo Virtual environment not found.
+    echo Please run install_venv.bat first.
+    pause
+    exit /b 1
+)
+
+call venv\Scripts\activate
+
+set PORT=5000
+for /f "tokens=2 delims=:" %%A in ('ipconfig ^| findstr /R /C:"IPv4 Address"') do (
+    set IP=%%A
+    goto :gotip
+)
+:gotip
+set IP=%IP:~1%
+echo Server will be available at http://%IP%:%PORT%
+
+python flask_app.py
