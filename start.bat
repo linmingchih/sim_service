bc1427959b00981716424c2b6663556c33cce088
Lin, Ming Chih <mingchih.lin8@gmail.com>
2025-07-06 12:23:35 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -10,12 +10,15 @@ if not exist "venv\Scripts\activate.bat" (
 call venv\Scripts\activate
 
 set PORT=5000
-for /f "tokens=2 delims=:" %%A in ('ipconfig ^| findstr /R /C:"IPv4 Address"') do (
-    set IP=%%A
-    goto :gotip
-)
-:gotip
-set IP=%IP:~1%
-echo Server will be available at http://%IP%:%PORT%
 
- python service\flask_app.py
+for /f "tokens=2 delims=:" %%A in ('ipconfig ^| findstr /R "IPv4 Address"') do (
+    set "rawip=%%A"
+    goto :parse_ip
+)
+
+:parse_ip
+set "IP=!rawip:~1!"
+
+echo Server will be available at http://!IP!:%PORT%
+
+python service\flask_app.py
